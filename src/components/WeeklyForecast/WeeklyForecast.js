40b8528b5118b9473f4adc1c54c66a572835d046
Briana Dodgen <brianadodgen@gmail.com>
2022-10-04 10:09:06 +0200

    completed

---
+++ src/components/WeeklyForecast/WeeklyForecast.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import DayForecast from '../DayForecast/DayForecast';
+
+const forecast = [
+  {
+    weekday: 'Mon',
+    weather: 'cloudy',
+    tempDay: 18,
+    tempNight: 11,
+  },
+  {
+    weekday: 'Tue',
+    weather: 'partly-cloudy',
+    tempDay: 21,
+    tempNight: 15,
+  },
+  {
+    weekday: 'Wed',
+    weather: 'rainy',
+    tempDay: 8,
+    tempNight: 1,
+  },
+  {
+    weekday: 'Thu',
+    weather: 'snowy',
+    tempDay: -2,
+    tempNight: -7,
+  },
+  {
+    weekday: 'Fri',
+    weather: 'stormy-rain',
+    tempDay: 28,
+    tempNight: 21,
+  },
+  {
+    weekday: 'Sat',
+    weather: 'stormy',
+    tempDay: 35,
+    tempNight: 27,
+  },
+  {
+    weekday: 'Sun',
+    weather: 'sunny',
+    tempDay: 28,
+    tempNight: 22,
+  },
+];
+
+
+export default function WeeklyForecast() {
+  return (
+    <>
+      <h1>This Week's Snazzy-ass Forecast</h1>
+      <div className="week-forecast">
+        {forecast.map((day) => {
+          return <DayForecast weekday={day.weekday} weather={day.weather} tempDay={day.tempDay} tempNight={day.tempNight} />
+        })
+        }
+      </div>
+    </>
+  )
+}
